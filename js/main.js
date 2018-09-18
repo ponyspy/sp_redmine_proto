f7eb1a6a375a787d71b00f3b03f0ca6f7c4bfe68
ponyspy <desade4me@gmail.com>
2018-09-18 10:17:38 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(function() {
 	$('.license_activates').on('click', function(e) {
 		$(this).parent().find('.activates_title, .license_activates_content').toggleClass('open');
 	});
